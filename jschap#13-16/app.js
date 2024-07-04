c779ba273d527f4eb13cc1ef8814e26feca623cb
Arfakanwal <Arfakanwal170@gmail.com>
2024-07-04 09:01:12 -0700

    completed

---
+++ jschap#13-16/app.js
@@ -0,0 +1,33 @@
+//ARRAYS
+//literal notation
+var studentNamesLiteral = [];
+//object notation
+var studentNamesObject = new Array();
+
+//STRING
+
+var stringArray=["Ayesha" ,"Abiha", "Bisma","Ayaan","Kamal"]
+document.write("<h1> STRING ARRAY</h1>"+ '<br/>');
+document.write(stringArray+"<br/>"  +"<br/>" );
+
+//NUMBERS 
+var heading=("<h1> NUMBER ARRAY</h1>"+ '<br/>')
+document.write(heading + "<br/>"  );
+var  numbersArray=[1,2,3,4,5,6,7,8,9 ]
+document.write(numbersArray  +'<br/>'  +"<br/>");
+
+//BOOLEAN 
+var heading=("<h1> BOOLEAN ARRAY</h1>"+ '<br/>')
+document.write(heading + "<br/>"  );
+var booleanArray = [true, false, true, false];
+document.write(booleanArray+`<br/>`  +"<br/>")
+
+//MIXED ARRAY
+var heading=("<h1> MIXED ARRAY</h1>"+ '<br/>')
+document.write(heading + "<br/>"  );
+var mixedArray = ["Hello", 42, true, null, undefined, {name: "John"}, [1, 2, 3]];
+document.write(mixedArray+`<br/>`  +"<br/>")
+//Qualification
+var Qualification=["SSC","HSC","BCS","BS","BCOM","MS","M.PHIL","PhD"];
+for(i=0; i<9; i++);
+
